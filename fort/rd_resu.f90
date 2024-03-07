62c118a91173f9758afb9a4c7744b6005dea5485
Giovanni Granucci <giovanni.granucci@unipi.it>
2024-03-07 08:47:15 +0100

    rd_resu evaluates <p> and <p**2>

--- fort/rd_resu.f90
+++ fort/rd_resu.f90
@@ -8,8 +8,9 @@ program rd_resu
   type (discr)        :: d
   complex (kind=dpc), allocatable, dimension (:,:) :: rw
   real (kind=dpr), allocatable, dimension (:)      :: r
+  real (kind=dpr), allocatable, dimension (:,:)    :: pval
   real (kind=dpr), allocatable, dimension (:)      :: ek,ep,pop
-  real (kind=dpr), allocatable, dimension (:,:)    :: e_pes,ravg,rsd
+  real (kind=dpr), allocatable, dimension (:,:)    :: e_pes,ravg,rsd,pavg,psd
   !
   narg = command_argument_count()
   if (narg <= 1) then
@@ -29,6 +30,7 @@ program rd_resu
   nrtot  = d%nrt(ndim)
   allocate (d%inddim(nrtot,ndim),d%fftkin(nrtot),d%fftkin2(nrtot))
   allocate (e_pes(nrtot,nstati))
+  allocate (pval(nrtot,ndim))
   read(1) d%inddim,e_pes
   !
   do i=1,ndim
@@ -39,11 +41,13 @@ program rd_resu
     endif
   end do
   call ind_fft_kin(d)
+  call ind_fft(d)
   drtot=aprod(1,ndim,d%dr)
   !
   allocate (rw(nrtot,nstati),r(ndim))
   allocate (ek(nstati),ep(nstati),pop(nstati))
   allocate (rsd(ndim,nstati),ravg(ndim,nstati))
+  allocate (psd(ndim,nstati),pavg(ndim,nstati))
   !
   open(2,file='WPDATA',form='formatted',status='unknown')
   rewind 2
@@ -68,11 +72,12 @@ program rd_resu
     if (icod /= 0) exit
     if (ti >= tini .and. ti < tend) then
        call ekin(rw,ek,ep,pop)
-       call rcalc(rw,pop,ravg,rsd)
+       call rcalc(rw,pop,ravg,rsd,pavg,psd)
        write(2,'(a,i8,a,f15.6,a,e18.9)')'   step=',k,'  time (fs)=',ti,' ekin=',sum(ek)
-       write(2,'(a)') ' State      pop       ekin           epot                  <r>            r_sd'
+       write(2,'(a)') ' State      pop       ekin           epot                  <r>            r_sd        <p>    p_sd'
        do i=1,nstati
-          write(2,'(i6,f12.6,50E18.9)') i,pop(i),ek(i),ep(i),(ravg(j,i),rsd(j,i),j=1,ndim)
+          write(2,'(i6,f12.6,*(E18.9))') i,pop(i),ek(i),ep(i),(ravg(j,i),rsd(j,i),j=1,ndim), &
+             & (pavg(j,i),psd(j,i),j=1,ndim)
        end do
        write(6,'(a,i8,a,f15.6,a,20f12.6)')'#   step=',k,'  time (fs)=',ti,' pop=',pop
        do i=1,nrtot
@@ -80,7 +85,7 @@ program rd_resu
          write(6,'(41f20.10)')r,(rw(i,a),a=1,nstati)
        end do
        if (rdiss > 0.0_dpr .and. ndim == 1) then
-          call diss(rw,rdiss,ek,ep,pop,ravg,rsd)
+          call diss(rw,rdiss,ek,ep,pop,ravg,rsd,pavg,psd)
           write(2,'(a,f12.6,a)') ' At dissociation (r >',rdiss,')'
           write(2,'(a)') ' State      pop       ekin           epot                  <r>            r_sd'
           write(2,'(i6,f12.6,4e18.9)') (i,pop(i),ek(i),ep(i),ravg(1,i),rsd(1,i),i=1,nstati)
@@ -113,12 +118,16 @@ contains
 
   end subroutine ekin
   !****************************************************************************
-  subroutine rcalc(rw,pop,ravg,rsd)
+  subroutine rcalc(rw,pop,ravg,rsd,pavg,psd)
+    use singleton
     implicit none
     complex (kind=dpc), dimension (:,:), intent (in) :: rw
     real (kind=dpr), dimension (:), intent (in)      :: pop
     real (kind=dpr), dimension (:,:), intent (inout) :: ravg,rsd
-    real (kind=dpr), dimension (nrtot)    :: r
+    real (kind=dpr), dimension (:,:), intent (inout) :: pavg,psd
+    real (kind=dpr), dimension (nrtot)         :: r,p
+    complex (kind=dpc), dimension (size(rw,1)) :: rwfft
+    integer, dimension (size(d%nr,1))          :: nrx
     integer :: alpha,jdim
 
     do jdim=1,ndim
@@ -129,23 +138,40 @@ contains
        end do
     end do
 
+    nrx=d%nr+1
+    do jdim=1,ndim
+       p(1:nrtot) = pval(1:nrtot,jdim)
+       do alpha=1,d%nstati
+          rwfft=rw(:,alpha)
+          call fftn(rwfft,nrx)
+          pavg(jdim,alpha)=sum(p(:)*abs(rwfft(:))**2)*drtot
+          psd(jdim,alpha)=sum(p(:)**2*abs(rwfft(:))**2)*drtot
+       end do
+    end do
+
     do alpha=1,d%nstati
        if (pop(alpha) < 1.e-10_dpr) then
           ravg(1:ndim,alpha) = 0.0_dpr
           rsd(1:ndim,alpha) = 0.0_dpr
+          pavg(1:ndim,alpha) = 0.0_dpr
+          psd(1:ndim,alpha) = 0.0_dpr
        else
           ravg(1:ndim,alpha) = ravg(1:ndim,alpha)/pop(alpha)
           rsd(1:ndim,alpha) = rsd(1:ndim,alpha)/pop(alpha)
+          pavg(1:ndim,alpha) = pavg(1:ndim,alpha)/pop(alpha)
+          psd(1:ndim,alpha) = psd(1:ndim,alpha)/pop(alpha)
        endif
     end do
     rsd=sqrt(rsd-ravg**2)
+    psd=sqrt(psd-pavg**2)
   end subroutine rcalc
   !****************************************************************************
-  subroutine diss(rw,rdiss,ek,ep,pop,ravg,rsd)
+  subroutine diss(rw,rdiss,ek,ep,pop,ravg,rsd,pavg,psd)
     implicit none
     complex (kind=dpc), dimension (:,:), intent (inout) :: rw
     real (kind=dpr), dimension (:), intent (inout)      :: ek,ep,pop
     real (kind=dpr), dimension (:,:), intent (inout)    :: ravg,rsd
+    real (kind=dpr), dimension (:,:), intent (inout)    :: pavg,psd
     real (kind=dpr), intent (inout)                     :: rdiss
     real (kind=dpr), dimension (nrtot)    :: r
     integer :: alpha
@@ -158,9 +184,47 @@ contains
     end do
 
     call ekin(rw,ek,ep,pop)
-    call rcalc(rw,pop,ravg,rsd)
+    call rcalc(rw,pop,ravg,rsd,pavg,psd)
 
   end subroutine diss
+  !****************************************************************************
+  subroutine ind_fft(d)
+    implicit none
+    type(discr), intent (inout) :: d 
+    complex (kind=dpc), dimension (d%ndim) :: dw
+    complex (kind=dpc)                     :: fac
+    real (kind=dpr), dimension (d%ndim)    :: dw2
+    real (kind=dpr)                        :: fac2
+    real (kind=dpr)                        :: pi,denom,due
+    integer, dimension (d%ndim)            :: nr1,nr2,ix,indp
+    integer                                :: i,j,ndim,nrtot
+    !
+    ndim=d%ndim
+    nrtot=d%nrt(ndim)
+    nr1(1:ndim)=d%nr(1:ndim)+1
+    nr2(1:ndim)=nr1(1:ndim)/2 
+    !
+    pi=4.0_dpr*atan(1.0_dpr)
+    due=2.0_dpr
+    fac=due*pi
+    do i=1,ndim
+      denom=nr1(i)*d%dr(i)
+      dw(i)=fac/denom
+    end do
+
+
+    do i=1,nrtot
+       ix=d%inddim(i,:)
+       do j=1,ndim
+         if (ix(j) > nr2(j)) then
+            indp(j)=ix(j)-nr1(j)-1
+         else
+            indp(j)=ix(j)-1
+         endif
+         pval(i,j)=dw(j)*indp(j)
+       end do
+    end do
+  end subroutine ind_fft
   !****************************************************************************
 end program rd_resu
 
